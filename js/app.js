240c18115f630a8ed6b9a5f5b900efa487c43327
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-12-15 22:06:20 -0300

    Agora o código está pegando todos os valores

--- js/app.js
+++ js/app.js
@@ -1,44 +1,41 @@
-
+//recuperar o tbody - preciso dele para criar a tr e as tds dos dados 
 const tbody = document.querySelector('tbody');
-console.log(tbody);
+// console.log(tbody);//pegar o form para cancelar o envio dos dados(EVENTO), 
+//quando o evento submit for disparado pare esse evento//para executar uma função 
+//que estará ligada ao formulário
 
-//pegar o form para cancelar o envio dos dados(EVENTO)
-//quando o evento submit for disparado, cancele esse evento
-//Para executar uma função que estará ligad ao formulário
-
-document.querySelector('form').addEventListener('submit', function(event){
-    //cancelar o evento padrão - receber na função o evento
+document.querySelector('form').addEventListener('submit', function(event) {
+    //cancelar o evento padrão - receber na função o evento    
     event.preventDefault();
-
-    //Recuperar os campos digitados do formulário e salvar em um array
-    const campos =[
+    // recuperar os campos do formulário - armazenar em um Array    
+    const campos = [
         document.querySelector('#usuario'),
         document.querySelector('#email'),
         document.querySelector('#dataCadastro'),
         document.querySelector('#tipoConta')
-        
     ];
+     console.log(campos);    
+     //criar a linha que receberá as colunas da tabela    
 
-    //criar a linha que receberá as colunas da tabela
-    const tr = document.createElement('tr');
+     const tr = document.createElement('tr');
+    // percorrer o array campos e pegar em cada um dos índices a informação digitada    
+    // cria um td  para cada campo    
+    // e coloca essa td na tr   
 
-
-    //Percorrer o array campos e pegar em cada um dos índices a informação digitada
-    //Cria uma td para cada campo
-    //e coloca essa td na tr
-    campos.forEach((campo)=>{
+    campos.forEach((campo) => {
+        //criar a coluna        
         const td = document.createElement('td');
 
-        //pegar o que foi digitado no campo
+        //pegar o que foi digitado no campo        
         td.textContent = campo.value;
 
-        //Adicionar a td na tr
+        // adicionar a td na tr        
         tr.appendChild(td);
-        //colocar no tbody a tr criada
-        tbody.appendChild(tr);
+    });
 
-        //limpar o formulário
-        this.reset();
-    })
+    // colocar no tbody a tr criada    
+    tbody.appendChild(tr);
 
+    //limpar o form    
+    this.reset();    
 });
